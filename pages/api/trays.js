2db6bcedbfd7262718ae8373ece9fa2b6e728c04
Franco Ortega <francopdx@gmail.com>
2022-08-12 13:14:40 -0700

    add trays endpoint with GET and POST requests

---
+++ pages/api/trays.js
@@ -0,0 +1,24 @@
+import clientPromise from '../../lib/mongodb';
+
+export default async function handler(req, res) {
+  console.log('trays endpoint');
+
+  const client = await clientPromise;
+
+  const db = client.db('eclipseDB');
+
+  switch (req.method) {
+    case 'GET':
+      const raw = await db.collection('trays').find({}).toArray();
+      const data = await JSON.parse(JSON.stringify(raw));
+      res.json(data);
+      break;
+    case 'POST':
+      const response = await db.collection('trays').insertOne(req.body);
+      res.json(response);
+      break;
+    default:
+      console.log('Method not available');
+      return;
+  }
+}
