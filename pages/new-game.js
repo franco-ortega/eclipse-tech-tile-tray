a081334840bb201cc7610fa8e2380e5ab1d87648
Franco Ortega <francopdx@gmail.com>
2022-08-13 09:17:09 -0700

    create New Game page

---
+++ pages/new-game.js
@@ -0,0 +1,37 @@
+import Home from '../components/home/Home';
+import clientPromise from '../lib/mongodb';
+
+export default function NewGame({ isConnected }) {
+  return (
+    <div>
+      <h2>New Game</h2>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise;
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // const client = await clientPromise;
+    // const db = client.db('eclipseDB');
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    // const raw = await db.collection('trays').find({}).toArray();
+
+    // const data = await JSON.parse(JSON.stringify(raw));
+
+    return {
+      props: { isConnected: true }
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: { isConnected: false }
+    };
+  }
+}
