0468cdb91a49db81b9b46a7eb072795f699c2a32
Franco Ortega <francopdx@gmail.com>
2022-08-13 10:35:45 -0700

    Select will display the new tray'

--- components/select/Select.jsx
+++ components/select/Select.jsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from 'react';
 import { useTrayContext } from '../../context/trayContext';
 import trayData from '../../data/tray.json';
+const { NEXT_PUBLIC_API_URL } = process.env;
 import Tray from '../tray/Tray';
 import styles from './Select.module.css';
 
 const Select = () => {
+  const [game, setGame] = useState(null);
   const { activeTray } = useTrayContext();
   console.log(activeTray);
+
+  useEffect(async () => {
+    await fetch(`${NEXT_PUBLIC_API_URL}/api/trays/?id=${activeTray}`)
+      .then((res) => res.json())
+      .then((res) => setGame(res));
+  }, []);
 
   return (
     <div className={styles.Select}>
       <h3>Click on tiles to add them to your tray.</h3>
-      <Tray active={false} name={trayData.name} rows={trayData.rows} />
+      {game && <Tray active={false} name={game.name} rows={game.rows} />}
     </div>
   );
 };
