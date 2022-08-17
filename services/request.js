01d21594ad46a23b5d39f6d42dc98839f4c06591
Franco Ortega <francopdx@gmail.com>
2022-08-16 18:31:44 -0700

    add request service to fetch GET, POST, PUT, DELETE

---
+++ services/request.js
@@ -0,0 +1,23 @@
+const { NEXT_PUBLIC_API_URL } = process.env;
+const METHODS_WITHOUT_BODY = ['GET', 'DELETE'];
+
+const request = async (method, path, data) => {
+  const headers = METHODS_WITHOUT_BODY.includes(method)
+    ? {}
+    : {
+        'Content-Type': 'application/json'
+      };
+
+  const response = await fetch(`${NEXT_PUBLIC_API_URL}${path}`, {
+    method,
+    headers,
+    body: JSON.stringify(data)
+  }).then((res) => res.json());
+
+  return response;
+};
+
+export const getData = (path) => request('GET', path);
+export const postData = (path, data) => request('POST', path, data);
+export const putData = (path, data) => request('PUT', path, data);
+export const deleteData = (path) => request('DELETE', path);
