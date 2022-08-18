b2a0e44ae4f5eef17adeabcfc4718a8c9fa98304
Franco Ortega <francopdx@gmail.com>
2022-08-18 14:20:02 -0700

    display available tiles in Slot component

--- components/slot/Slot.jsx
+++ components/slot/Slot.jsx
@@ -4,9 +4,11 @@ import styles from './Slot.module.css';
 const Slot = ({ active, category, color, tile }) => {
   return (
     <li className={styles.Slot}>
-      {!active && tile && (
+      {!active && tile ? (
         <Tile active={active} category={category} color={color} tile={tile} />
-      )}
+      ) : active && tile.selected - tile.used ? (
+        <Tile active={active} category={category} color={color} tile={tile} />
+      ) : null}
     </li>
   );
 };
