b4d978d1dd00553c760a453e3f0880ae318ff92f
Franco Ortega <francopdx@gmail.com>
2022-08-19 00:21:54 -0700

    remove code that is no longer used

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -17,21 +17,6 @@ const Home = () => {
       <section>
         <NewGameButton length={trays.length} />
         <TrayList trays={trays} />
-        {/* <select name='trays' id='trays' onChange={onSelectGame}>
-          <option value=''>Select Game</option>
-          {trays &&
-            trays
-              .sort((a, b) => {
-                if (a.date > b.date) return -1;
-                if (a.date < b.date) return 1;
-                return 0;
-              })
-              .map((tray) => (
-                <option key={tray._id} value={tray._id}>
-                  {tray.name}
-                </option>
-              ))}
-        </select> */}
       </section>
     </div>
   );
