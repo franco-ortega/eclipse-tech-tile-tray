532cdd5db20a34d0525c545720fc7f1e434f52f0
Franco Ortega <francopdx@gmail.com>
2022-08-19 00:19:17 -0700

    refactor select element with list of trays into TrayList component

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,45 +1,23 @@
-import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { useTrayContext } from '../../context/trayContext';
 import { getData } from '../../services/request';
-import { setLocalStorage } from '../../utils/localStorage';
 import NewGameButton from '../buttons/NewGameButton';
+import TrayList from '../trayList/TrayList';
 import styles from './Home.module.css';
 
 const Home = () => {
   const [trays, setTrays] = useState([]);
-  const { setActiveTrayId } = useTrayContext();
-  const router = useRouter();
 
   useEffect(async () => {
     const response = await getData('/api/trays');
     await setTrays(response);
   }, []);
 
-  const onSelectGame = async (e) => {
-    if (!e.target.value) {
-      alert('Please select a game.');
-      return;
-    }
-
-    const response = await getData(`/api/trays/?id=${e.target.value}`);
-
-    const proceedToGame = confirm(
-      `You have selected ${response.name}. Click OK to proceed.`
-    );
-
-    if (proceedToGame) {
-      setActiveTrayId(response._id);
-      setLocalStorage('ACTIVE_TRAY_ID', response._id);
-      router.push('/play-game');
-    } else alert('Please select another game.');
-  };
-
   return (
     <div className={styles.Home}>
       <section>
         <NewGameButton length={trays.length} />
-        <select name='trays' id='trays' onChange={onSelectGame}>
+        <TrayList trays={trays} />
+        {/* <select name='trays' id='trays' onChange={onSelectGame}>
           <option value=''>Select Game</option>
           {trays &&
             trays
@@ -53,7 +31,7 @@ const Home = () => {
                   {tray.name}
                 </option>
               ))}
-        </select>
+        </select> */}
       </section>
     </div>
   );
