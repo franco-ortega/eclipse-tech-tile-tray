9bce7b26eb7c2e3816e0730217c68ef27e6a5024
Franco Ortega <francopdx@gmail.com>
2022-08-19 14:21:43 -0700

    remove unused code

--- pages/new-round.js
+++ pages/new-round.js
@@ -1,11 +1,9 @@
-import StartRoundButton from '../components/buttons/StartRoundButton';
 import SelectTiles from '../components/selectTiles/SelectTiles';
 
 export default function NeRound() {
   return (
     <div>
       <SelectTiles active={false} />
-      {/* <StartRoundButton /> */}
     </div>
   );
 }
