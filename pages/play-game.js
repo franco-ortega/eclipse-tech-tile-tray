e28bc3bfb93d113080cfd90040a5a40e3eecc955
Franco Ortega <francopdx@gmail.com>
2022-08-19 11:53:48 -0700

    move play-game buttons into Play component

--- pages/play-game.js
+++ pages/play-game.js
@@ -1,22 +1,9 @@
 import Play from '../components/play/Play';
-import NextRoundButton from '../components/buttons/NextRoundButton';
-import EndGameButton from '../components/buttons/EndGameButton';
 
 export default function PlayGame() {
   return (
     <div>
       <Play active={true} />
-      {/* <div
-        style={{
-          display: 'flex',
-          gap: '2rem',
-          width: 'fit-content',
-          marginInline: 'auto'
-        }}
-      > */}
-      <NextRoundButton />
-      <EndGameButton />
-      {/* </div> */}
     </div>
   );
 }
