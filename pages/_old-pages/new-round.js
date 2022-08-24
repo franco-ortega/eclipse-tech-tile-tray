d1a4da0bef3d3c48b995c9a4b96f1780ee53ec77
Franco Ortega <francopdx@gmail.com>
2022-08-23 17:26:19 -0700

    delete old pages

--- pages/_old-pages/new-round.js
+++
@@ -1,9 +0,0 @@
-import SelectTiles from '../../components/selectTiles/SelectTiles';
-
-export default function NewRound() {
-  return (
-    <div>
-      <SelectTiles active={false} />
-    </div>
-  );
-}
--- pages/_old-pages/play-game.js
+++
@@ -1,9 +0,0 @@
-import Play from '../../components/play/Play';
-
-export default function PlayGame() {
-  return (
-    <div>
-      <Play active={true} />
-    </div>
-  );
-}
