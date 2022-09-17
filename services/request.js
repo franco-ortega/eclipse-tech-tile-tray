1169963270e153edf87542b390f67cb64ca4dd58
Franco Ortega <francopdx@gmail.com>
2022-09-16 19:50:41 -0700

    refactor DELETE request to receive body

--- services/request.js
+++ services/request.js
@@ -1,5 +1,5 @@
 const { NEXT_PUBLIC_API_URL } = process.env;
-const METHODS_WITHOUT_BODY = ['GET', 'DELETE'];
+const METHODS_WITHOUT_BODY = ['GET'];
 
 const request = async (method, path, data) => {
   const headers = METHODS_WITHOUT_BODY.includes(method)
@@ -20,4 +20,4 @@ const request = async (method, path, data) => {
 export const getData = (path) => request('GET', path);
 export const postData = (path, data) => request('POST', path, data);
 export const putData = (path, data) => request('PUT', path, data);
-export const deleteData = (path) => request('DELETE', path);
+export const deleteData = (path, data) => request('DELETE', path, data);
